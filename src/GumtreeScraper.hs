eb6a75fe602419634b062b506fc5a9fdf2b2337f
Marcin Moskal <mmos@memleak.pl>
2019-06-19 17:15:41 +0200

    new schema [skip ci]

--- src/GumtreeScraper.hs
+++ src/GumtreeScraper.hs
@@ -7,23 +7,26 @@ module GumtreeScraper
 import Text.HTML.Scalpel
 import Data.Text (Text)
 import Data.Maybe (Maybe(Nothing))
-import Data.Time (UTCTime)
 
 import Offer
 import WordUtils
 
-offerScraper :: UTCTime -> Scraper Text Offer
-offerScraper timestamp = do
+offerScraper :: BasicOffer -> Scraper Text Offer
+offerScraper offer = do
   let titleElement = "div" @: [hasClass "title"]
   name <- stripSpaces . dropShitwords <$> (text $ titleElement)
   url <- ("https://www.gumtree.pl" <>) <$> attr "href" (titleElement // "a")
   price <- parsePrice <$> text ("span" @: [hasClass "ad-price"])
-  return $ basicOffer name price url timestamp "gumtree.pl"
+  return $ offer name price url
 
-offersScraper :: UTCTime -> Scraper Text [Offer]
-offersScraper timestamp = chroots
+offersScraper :: BasicOffer -> Scraper Text [Offer]
+offersScraper offer = chroots
   ("div" @: [hasClass "view"] // "div" @: [hasClass "tileV1"])
-  (offerScraper timestamp)
+  (offerScraper offer)
 
 gumtreeScraper :: Config Text -> OfferScraper
-gumtreeScraper config = OfferScraper config offersScraper Nothing
+gumtreeScraper config = OfferScraper
+  config
+  (basicOffer "gumtree.pl")
+  offersScraper
+  Nothing
