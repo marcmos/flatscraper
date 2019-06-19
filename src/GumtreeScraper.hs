e490aba89ef44ca12be38bf0b7eadab5c967bbd9
Marcin Moskal <mmos@memleak.pl>
2019-06-19 18:23:39 +0200

    private offers [ci skip]

--- src/GumtreeScraper.hs
+++ src/GumtreeScraper.hs
@@ -6,13 +6,18 @@ module GumtreeScraper
 
 import Text.HTML.Scalpel
 import Data.Text (Text)
-import Data.Maybe (Maybe(Nothing))
 
 import Offer
 import WordUtils
 
-offerScraper :: BasicOffer -> Scraper Text Offer
-offerScraper offer = do
+detailedOfferScraper :: Offer -> Scraper Text Offer
+detailedOfferScraper offer = chroot ("div" @: [hasClass "vip-details"]) $ do
+  offerAttrs <- texts ("div" @: [hasClass "attribute"] // "span" @: [hasClass "value"])
+  direct <- return . Just $ (not . elem "Agencja") (stripSpaces <$> offerAttrs)
+  return offer { offerDirect = direct }
+
+listOfferScraper :: BasicOffer -> Scraper Text Offer
+listOfferScraper offer = do
   let titleElement = "div" @: [hasClass "title"]
   name <- stripSpaces . dropShitwords <$> (text $ titleElement)
   url <- ("https://www.gumtree.pl" <>) <$> attr "href" (titleElement // "a")
@@ -22,11 +27,11 @@ offerScraper offer = do
 offersScraper :: BasicOffer -> Scraper Text [Offer]
 offersScraper offer = chroots
   ("div" @: [hasClass "view"] // "div" @: [hasClass "tileV1"])
-  (offerScraper offer)
+  (listOfferScraper offer)
 
 gumtreeScraper :: Config Text -> OfferScraper
 gumtreeScraper config = OfferScraper
   config
   (basicOffer "gumtree.pl")
   offersScraper
-  Nothing
+  (Just detailedOfferScraper)
