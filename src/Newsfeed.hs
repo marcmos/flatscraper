3c16925d37f2d85cf63373b7d7123f36e8a13889
Marcin Moskal <mmos@memleak.pl>
2021-03-12 18:28:22 +0100

    Add price per square meter to tag + reformat

--- src/Newsfeed.hs
+++ src/Newsfeed.hs
@@ -1,18 +1,17 @@
 {-# LANGUAGE OverloadedStrings #-}
 
 module Newsfeed
-  ( renderOfferFeed
-  ) where
+  ( renderOfferFeed,
+  )
+where
 
-import Data.Text as T (pack, intercalate, Text, concat)
+import Data.Maybe (catMaybes, fromMaybe)
+import Data.Text as T (Text, intercalate, pack)
 import qualified Data.Text.Lazy as TL (Text)
-import Text.RSS.Syntax
-import Text.RSS.Export
 import Data.Time
-import Data.Monoid ((<>))
-import Data.Maybe (fromMaybe)
-
 import Offer
+import Text.RSS.Export
+import Text.RSS.Syntax
 
 extraTag :: OfferExtra -> T.Text
 extraTag KitchenAnnex = "anx"
@@ -24,8 +23,8 @@ extraTag Climatronic = "kli"
 extraTag Balcony = "bal"
 
 ownerTag :: Offer -> [T.Text]
-ownerTag Offer { offerOwnerOffer = Just True } = ["pry"]
-ownerTag Offer { offerOwnerOffer = Just False } = ["age"]
+ownerTag Offer {offerOwnerOffer = Just True} = ["pry"]
+ownerTag Offer {offerOwnerOffer = Just False} = ["age"]
 ownerTag _ = []
 
 extraDescription :: OfferExtra -> T.Text
@@ -38,17 +37,18 @@ extraDescription Balcony = "balkon"
 extraDescription Internet = "internet"
 
 ownerDescription :: Offer -> [T.Text]
-ownerDescription Offer { offerOwnerOffer = Just True } = ["oferta prywatna"]
-ownerDescription Offer { offerOwnerOffer = Just False } = ["oferta z biura/agencji"]
+ownerDescription Offer {offerOwnerOffer = Just True} = ["oferta prywatna"]
+ownerDescription Offer {offerOwnerOffer = Just False} = ["oferta z biura/agencji"]
 ownerDescription _ = []
 
 offerTag :: Offer -> T.Text
 offerTag offer =
   if null tags then "" else "[" <> T.intercalate "," tags <> "]"
-  where tags =
-          maybe [] (return . T.pack . show) (offerRooms offer) <>
-          ownerTag offer <>
-          (extraTag <$> offerExtras offer)
+  where
+    tags =
+      maybe [] (return . T.pack . show) (offerRooms offer)
+        <> ownerTag offer
+        <> (extraTag <$> offerExtras offer)
 
 agencyFee :: Offer -> Maybe Int
 agencyFee offer = do
@@ -71,44 +71,72 @@ offerDescriptionTexts offer =
   -- maybe [] (return . ("odstępne: " <>) . T.pack . show) (offerRentPrice offer) <>
   -- maybe [] (return . ("narzut agencji (estymowany): " <>) . T.pack . show) (agencyFee offer) <>
   -- maybe [] (return . ("cena za mc: " <>) . T.pack . show) (monthlyPrice offer) <>
-  ownerDescription offer <>
-  maybe [] return (roomsDescription offer) <>
-  maybe [] return (areaDescription offer) <>
-  (extraDescription <$> filter (/= SeparateRooms) (offerExtras offer))
+  ownerDescription offer
+    <> maybe [] return (roomsDescription offer)
+    <> maybe [] return (areaDescription offer)
+    <> (extraDescription <$> filter (/= SeparateRooms) (offerExtras offer))
 
 roomsDescription :: Offer -> Maybe T.Text
 roomsDescription offer =
-  (\p -> "pokoje" <> (if SeparateRooms `elem` offerExtras offer then " osobne(?): " else ": ") <> p) .
-  (T.pack . show) <$> offerRooms offer
+  (\p -> "pokoje" <> (if SeparateRooms `elem` offerExtras offer then " osobne(?): " else ": ") <> p)
+    . (T.pack . show)
+    <$> offerRooms offer
 
 areaDescription :: Offer -> Maybe T.Text
 areaDescription offer = (\x -> T.pack "metraż: " <> (T.pack . show) x) <$> offerArea offer
 
 areaTag :: Offer -> T.Text
-areaTag offer = maybe " " (\x -> T.concat [T.pack " [", T.pack . show $ x,  T.pack "] "]) (offerArea offer)
+areaTag offer =
+  " ["
+    <> T.intercalate
+      ", "
+      ( catMaybes
+          [ (<> "m2") . T.pack . show <$> offerArea offer,
+            pricePerSquareMeter offer,
+            Just . attrText $ offer
+          ]
+      )
+    <> "] "
 
-renderOfferFeedEntry :: Offer -> RSSItem
-renderOfferFeedEntry offer = (nullItem title)
-  { rssItemLink = Just $ offerURL offer
-  , rssItemPubDate = Just . T.pack . formatTime defaultTimeLocale rfc822DateFormat . offerVisit $ offer
-  , rssItemDescription = Just . T.intercalate ", " $ offerDescriptionTexts offer
-  , rssItemAuthor = Just $ offerScraperName offer
-  }
+attrText :: Offer -> T.Text
+attrText = T.pack . show . length . offerExtras
+
+pricePerSquareMeter :: Offer -> Maybe T.Text
+pricePerSquareMeter offer = (\area -> T.pack . show $ price `div` area) <$> offerArea offer
   where
     price = offerPrice offer
-    title = (case offerRentPrice offer of
+
+renderOfferFeedEntry :: Offer -> RSSItem
+renderOfferFeedEntry offer =
+  (nullItem title)
+    { rssItemLink = Just $ offerURL offer,
+      rssItemPubDate = Just . T.pack . formatTime defaultTimeLocale rfc822DateFormat . offerVisit $ offer,
+      rssItemDescription = Just . T.intercalate ", " $ offerDescriptionTexts offer,
+      rssItemAuthor = Just $ offerScraperName offer
+    }
+  where
+    price = offerPrice offer
+    title =
+      ( case offerRentPrice offer of
           Just rentPrice ->
-            (T.pack . show) price <> "+" <>
-            (T.pack . show) rentPrice <> "=" <>
-            (T.pack . show) (price + rentPrice)
-          Nothing        -> T.pack . show $ offerPrice offer)
-          <> areaTag offer <> offerTitle offer
+            (T.pack . show) price <> "+"
+              <> (T.pack . show) rentPrice
+              <> "="
+              <> (T.pack . show) (price + rentPrice)
+          Nothing -> T.pack . show $ offerPrice offer
+      )
+        <> areaTag offer
+        <> offerTitle offer
 
 -- FIXME nullRSS empty strings replaced later
 renderOfferFeed :: [Offer] -> Maybe TL.Text
 renderOfferFeed offers =
-  textRSS $ (nullRSS "" "") {rssChannel = (nullChannel "flatscraper" url)
-                              {rssItems = renderOfferFeedEntry <$> offers}
-                            }
-  where url = "http://heap.memleak.pl/mmos/newsfeed.xml"
-
+  textRSS $
+    (nullRSS "" "")
+      { rssChannel =
+          (nullChannel "flatscraper" url)
+            { rssItems = renderOfferFeedEntry <$> offers
+            }
+      }
+  where
+    url = "http://heap.memleak.pl/mmos/newsfeed.xml"
