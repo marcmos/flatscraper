f77c9440a4124ed53ff12ffb54b4aefe9d063d84
Marcin Moskal <mmos@memleak.pl>
2021-03-13 21:30:25 +0100

    Update blacklist

--- src/OfferFilter.hs
+++ src/OfferFilter.hs
@@ -13,11 +13,59 @@ bannedLocations = [
   , "bronowic"
   , "krowodrz"
   , "dębnik"
-  , "prądnik"
+  , "prądni"
   , "justowsk"
-  , "kazimierz"
-  , "hut"
   , "azor"
+  , "kurdwan"
+  , "salwator"
+  , "duchack"
+  , "stare miasto" -- use only for title filtering
+  , "rakowic"
+  , "łobz"
+  , "zwierzy" -- zwierzyniec
+  , "prokoci"
+
+  , "kazimierz" -- corner case: galeria kazimierz
+  , "stradom"
+  , "starowiśln"
+
+  , "kleparz"
+  , "wrocławs"
+  , "mazowiecka"
+
+  , "centrum a", "centruma"
+  , "centrum b", "centrumb"
+  , "centrum c", "centrumc"
+  , "centrum d", "centrumd"
+
+  , " hut"
+  , " hucie"
+  , "borek"
+  , "fałę"
+  , "łagiew"
+
+  , "błoń"
+  , "błoni"
+
+  , " mak"
+  , "europejski"
+
+  , "29 listopada"
+  , "opolsk"
+  , "prandoty"
+  , "olsz"
+  , "żabi"
+  , "dobrego pasterza"
+
+  , "podwawels"
+
+  , "krzeszow"
+
+  , "quattro"
+  , "nokia"
+  , "motorola"
+  , "serenad"
+
   ]
 
 allFieldsTextPredicate :: [Text] -> Offer -> Bool
